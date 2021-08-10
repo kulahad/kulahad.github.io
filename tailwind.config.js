8c23e0680d13093591618d28af312e4d162da4e1
KulAhad <kulahad007@gmail.com>
2021-08-10 23:52:28 +1200

    feat: update tailwind css config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,40 @@
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  purge: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./shared/**/*.{js,ts,jsx,tsx}",
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#002147",
+        secondary: "#472600",
+        tertiary: "#C76A00",
+      },
+      fontFamily: {
+        primary: ["Open Sans", "serif"],
+        secondary: ["Lato", "sans-serif"],
+      },
+      minHeight: {
+        "1/4-screen": "25vh",
+        "1/3-screen": "33vh",
+        "1/2-screen": "50vh",
+        "3/4-screen": "75vh",
+      },
+      maxHeight: {
+        "1/4-screen": "25vh",
+        "1/3-screen": "33vh",
+        "1/2-screen": "50vh",
+        "3/4-screen": "75vh",
+      },
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      rotate: ["group-hover"],
+      backgroundColor: ["odd"],
+    },
   },
   plugins: [],
 };
