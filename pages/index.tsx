b2a6345ab7482efab39e1aeab3249a50453913fa
Mohammed Muqtadir Ahad <50536283+kulahad@users.noreply.github.com>
2021-08-26 01:19:07 +1200

    fix: undo last commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -160,9 +160,6 @@ export default function Home() {
             <MdEmail />
           </a> */}
               </div>
-              <div>
-                <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1306585441&color=%23ff0000&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/user-14790367" title="Kulahad" target="_blank" style="color: #cccccc; text-decoration: none;">Kulahad</a> · <a href="https://soundcloud.com/user-14790367/sets/programming-with-chill-vibes" title="Programming with Chill Vibes" target="_blank" style="color: #cccccc; text-decoration: none;">Programming with Chill Vibes</a></div>
-              </div>
             </div>
           </div>
           <div
