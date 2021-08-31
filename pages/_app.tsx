5ee6f25b3b6df65af3069d67c978b38951926fb1
KulAhad <kulahad007@gmail.com>
2021-09-01 00:14:29 +1200

    feat: add `logrocket`

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import LogRocket from "logrocket";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    LogRocket.init("apfj24/portfolio");
+  }, []);
+
   return <Component {...pageProps} />;
 }
 export default MyApp;
